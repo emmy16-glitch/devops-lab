ede57161225bcfbc70d8786bcea4584f04a5dce8
Emmanuel Okunlola <emmanuelokunlola16@gmail.com>
2026-09-25 16:19:29 +0000

    Add version endpoint

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get("/health", (req, res) => {
     res.json({ status: "ok" });
 });
 
+app.get("/version", (req, res) => {
+    res.json({
+        version: "1.0.0"
+    });
+
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
