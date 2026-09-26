44bf403649f012a78091e2128fea6fa2a054139b
EMMANUEL AYOMIDE OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-09-26 09:29:51 +0100

    Use Render-provided port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("My DevOps Lab is running!");
@@ -15,8 +15,8 @@ app.get("/version", (req, res) => {
     res.json({
         version: "2.0.1"
     });
-
 });
+
 if (require.main === module) {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
